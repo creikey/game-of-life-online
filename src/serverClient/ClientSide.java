a0ed771b8e1a09debad31b70b919726418ae7c01
Cameron Reikes <github@mrspapaman.33mail.com>
2017-08-04 14:31:29 -0700

    Updated client to add eror checking

--- src/serverClient/ClientSide.java
+++ src/serverClient/ClientSide.java
@@ -3,16 +3,24 @@ package serverClient;
 import processing.core.PApplet;
 import processing.net.*;
 
-public class ClientSide {
-	String ip = "127.0.0.1";
+public class ClientSide extends PApplet{
+	String ip = "192.168.43.106";
 	int port = 5204;
 	Client myClient;
 	boolean[][] patternDraw;
 	byte[] patternBuffer;
 
 	public void runClient( PApplet engine ) {
+		String clientStatus = "Good, but waiting";
+		if( myClient == null ) {
+			myClient = new Client(this, ip, port);
+		}
+		if( myClient.active() == false ) {
+			myClient = new Client(this, ip, port);
+			clientStatus = "Not so good...";
+		}
 		engine.background(0);
-		engine.text("Client side connection status: ", 250, 30);
+		engine.text("Client side connection status: " + clientStatus, 250, 30);
 	}
 	
 }
